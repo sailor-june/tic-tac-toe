07e58f70810bdeef1abc25701521926670c87965
Juniper James <vvlpespro@gmail.com>
2022-11-25 09:38:40 -0500

    slight tweaks

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ let turn= 1
 
 for (let i = 0 ;i<9 ;i++){
     let newBtn = $(`<div class="btn" id="${i}"></div>`)
+    $('.btn').addClass('open')
     $('#main').append(newBtn)
     
 }
 
 
 $('.btn').on('click', function(evt){
-    if (!evt.target.classList.contains('player')){
-    evt.target.classList.add('player')}
-    turn = turn * -1
-    
-    $('#state').text(turn)}
-)
+    if (evt.target.classList.contains('open')){
+        if (turn == 1){
+            evt.target.classList.remove('open')
+            evt.target.classList.add('player-one')
+            turn = turn * -1
+
+    }
+    else { 
+            evt.target.classList.remove('open')
+            evt.target.classList.add('player-two')
+            turn = turn*-1
+        }
+    $('#state').text(turn)
+}
+else{return}
+
+})
 
 $('#reset').on('click', function(evt){
-    $('.player').removeClass('player')
+    $('.player-one').removeClass('player-one')
+    $('.player-two').removeClass('player-two')
+    $('.btn').addClass('open')
+    turn = 1
+    $('#state').text(turn)
 })
 
 $('#state').text(turn)
